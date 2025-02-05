c8284ffbcdee0cb559c5be9e4f76063ce8c9fc99
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-02-05 10:55:36 -0500

    Add an empty MAUI template app for the intearctive version of the application

---
+++ LibraryApp_Interactive/LibraryAppInteractive/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAppInteractive;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ LibraryApp_Interactive/LibraryAppInteractive/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryAppInteractive;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
